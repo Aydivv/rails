a2131e482de2625f38d77a7ae2cc4fd968df87d8
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-05-30 19:47:11 +0100

    react api works

--- bookable/src/App.js
+++ bookable/src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import Books from './components/Books';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const API_URL = "http://127.0.0.1:3000/api/v1/books";
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data);
+}
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => {
+      if(mounted) {
+        setBooks(items);
+        console.log(items);
+      }
+    });
+
+    return () => (mounted = false)
+  }, []);
+  
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Books books={books} />
     </div>
   );
 }
